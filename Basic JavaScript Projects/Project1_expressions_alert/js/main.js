922fbc5d17845e2c8a63499a2c92c6cd83a4184e
dnlwrn01 <78996771+dnlwrn01@users.noreply.github.com>
2021-03-01 20:49:35 -0600

    Created Basic JavaScript Page

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,22 @@
+// alert('Hello World!'); 
+
+window.alert('Hello World'); // Popup Message 
+document.write('Hello again?'); // Write text on page
+
+var hello = 'Don\'t say hello to me lol'; // Variable Declaration 
+window.alert(hello); // display variable string on screen 
+
+// String Concatenation
+document.write('Hello! '
++ 'My name is Daniel '
++ 'This is call concatenating a string.'
+);
+
+var test = ('Concatenated' + ' String');
+document.write (test);
+
+var drinkType = 'Latte', carShot = 'Caramel Syrup', vanShot = 'Vanilla Syrup', hazShot = 'Hazelnut Syrup';
+var vanShot = vanShot.fontcolor('blue'); // set font color to blue
+document.write(vanShot);
+
+Document.write(3+3);
