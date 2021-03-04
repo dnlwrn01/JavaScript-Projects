14fa7eb006c3982d6e93402f4d36fbb599f13752
dnlwrn01 <78996771+dnlwrn01@users.noreply.github.com>
2021-03-03 21:00:46 -0600

    Created Project 2/3
    String Concatenation, Variable Declaration, Operators +=

---
+++ Basic JavaScript Projects/Project2_Functions/js/main.js
@@ -0,0 +1,11 @@
+function colorChangeG() {
+    var str = "Green Text";
+    var result = str.fontcolor('green');
+    document.getElementById('green-text').innerHTML = result;
+}
+
+function colorChangeB() {
+    var str = "Who's There?"
+    var result = str.fontcolor('blue');
+    document.getElementById('links-hover').innerHTML = result;
+}
---
+++ Basic JavaScript Projects/Project3_OperatorAssign/js/main.js
@@ -0,0 +1,5 @@
+function stringC() { //create function 'stringC()'
+    var sentence = "My name is"; //declare variable and set to string
+    sentence += " Daniel Warren"; //concatenate string variable
+    document.getElementById('Concatenate').innerHTML = sentence; //set content of ID to variable sentenc
+}
